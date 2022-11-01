9b1748ae18bcf12eef57aa17041f9415999bd9f1
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-11-01 13:53:59 -0400

    Restarting Repo for NextJS

--- frontend/src/pages/Friendz/friendz.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Friendz() {
-    return (
-        <div>
-            
-        </div>
-    );
-};
--- frontend/src/pages/Messaging/Sent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Sent() {
-    return (
-        <div>
-            
-        </div>
-    );
-};
