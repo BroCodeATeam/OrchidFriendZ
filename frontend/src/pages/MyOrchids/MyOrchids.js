bb0358cc169770c8d0e0fa3c7cc633cc36eafee3
Ryan Howard <ryanghoward@protonmail.com>
2022-10-31 17:07:16 -0700

    Fixed minor Tailwind issue

--- frontend/src/pages/MyOrchids/MyOrchids.js
+++ frontend/src/pages/MyOrchids/MyOrchids.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { IconButton } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
 
 function MyOrchids() {
   return (
-    <div className='flex items-center w-max gap-4'>
-      {/* <Button color='blue'>color blue</Button>
-      <Button color='red'>color red</Button>
-      <Button color='green'>color green</Button>
-      <Button color='amber'>color amber</Button> */}
-      <Button size='lg'>Button</Button>
-    </div>
+    <>
+      <div className='flex w-max gap-4 mt-4'>
+        <Button variant='filled'>filled</Button>
+        <Button variant='gradient'>gradient</Button>
+        <Button variant='outlined'>outlined</Button>
+        <Button variant='text'>text</Button>
+      </div>
+      <div className='flex w-max gap-4 mt-4'>
+        <Button size='sm'>small</Button>
+        <Button size='md'>medium</Button>
+        <Button size='lg'>large</Button>
+      </div>
+      <div className='flex w-max gap-4 mt-4'>
+        <Button color='blue'>color blue</Button>
+        <Button color='red'>color red</Button>
+        <Button color='green'>color green</Button>
+        <Button color='amber'>color amber</Button>
+      </div>
+      <div className='flex w-max gap-4 mt-4'>
+        <Button fullWidth>Full Width Button</Button>
+      </div>
+      <div className='flex w-max gap-4 mt-4'>
+        <Button ripple={true}>Ripple Effect On</Button>
+        <Button ripple={false}>Ripple Effect Off</Button>
+      </div>
+    </>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
