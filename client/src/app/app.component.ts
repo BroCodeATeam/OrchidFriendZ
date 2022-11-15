cb2f71ea1a3c1c011b2b125a3191800475e1837a
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-11-15 17:40:35 -0500

    Fuckin nailed it!

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 // Material Imports
 import { MatBottomSheet } from '@angular/material/bottom-sheet'
+import {AuthService} from "./shared/services/auth";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,12 @@ export class AppComponent {
   title = 'client';
 
   constructor(
-    private loginSheet: MatBottomSheet
-  ){
+    private loginSheet: MatBottomSheet,
+    public authService: AuthService
+  ){}
 
+  loggedIn(){
+    return this.authService.isLoggedIn
   }
+
 }
--- client/src/app/shared/guard/auth.guard.ts
+++ client/src/app/shared/guard/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if(this.authService.isLoggedIn !== true) {
-      this.router.navigate(['authenticator'])
+      this.router.navigate(['home'])
     }
     return true;
   }
