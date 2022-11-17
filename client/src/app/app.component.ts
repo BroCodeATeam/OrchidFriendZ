92bfc3e4eb9664dff97dd147add48f74e26e4445
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-11-17 13:57:51 -0500

    Restarting Fresh Repo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 
-// Material Imports
-import { MatBottomSheet } from '@angular/material/bottom-sheet'
-import {AuthService} from "./shared/services/auth";
-import {AuthenticatorComponent} from "./tools/authenticator/authenticator.component";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,20 +7,4 @@ import {AuthenticatorComponent} from "./tools/authenticator/authenticator.compon
 })
 export class AppComponent {
   title = 'client';
-
-  constructor(
-    public loginSheet: MatBottomSheet,
-    public authService: AuthService
-  ){}
-
-  onLoginClick(){
-    this.loginSheet.open(AuthenticatorComponent)
-    this.loginSheet.dismiss(AuthenticatorComponent)
-  }
-
-  loggedIn(){
-    return this.authService.isLoggedIn
-    this.loginSheet.dismiss(AuthenticatorComponent)
-  }
-
 }
